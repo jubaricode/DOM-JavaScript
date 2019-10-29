2e0251c2b91334a251db860d49e8b149919b4f9b
Jubari <jubari.code@gmail.com>
2019-10-29 21:26:53 +0700

    Tutorial DOM JavaScript - 05 DOM Selector untuk Memilih Banyak Element Bag 1

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-// Single Selector
-// document.getElementById()
+// // document.getElementsByClassName
 
-console.log(document.getElementById('task-title'));
+// const items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[2].textContent = 'Hello';
 
-console.log(document.getElementById('task-title').id);
-console.log(document.getElementById('task-title').className);
+// const listItems = document.querySelector('ul').getElementsByClassName('list-group-item');
+// console.log(listItems);
 
-// //Change Styling
-// document.getElementById('task-title').style.background= '#333333';
-// document.getElementById('task-title').style.color= '#ffffff';
-// document.getElementById('task-title').style.padding = '5px';
+// document.getElementsByTagName
 
-// // Change Content
-// document.getElementById('task-title').textContent = 'Task Table';
-// document.getElementById('task-title').innerText = 'My Tasks';
-// document.getElementById('task-title').innerHTML = '<span style="color: red">Task List</span>';
-
-const cardHeader = document.getElementById('task-title');
-//Change Styling
-cardHeader.style.background= '#333333';
-cardHeader.style.color= '#ffffff';
-cardHeader.style.padding = '5px';
-
-// Change Content
-cardHeader.textContent = 'Task Table';
-cardHeader.innerText = 'My Tasks';
-cardHeader.innerHTML = '<span style="color: red">Task List</span>';
-
-// document.querySelector()
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-header'));
-console.log(document.querySelector('button'));
-console.log(document.querySelector('.delete-item'));
-
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
-
-document.querySelector('li:last-child').style.color = 'yellow';
-document.querySelector('li:nth-child(2)').style.color = 'purple';
-document.querySelector('li:nth-child(2)').textContent = 'Hello World!';
+const lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = 'red';
+lis[2].textContent = 'Hello';
