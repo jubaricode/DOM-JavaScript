31b4cb1ce8ac493944845297ae7c4cb1539e1eb1
Jubari <jubari.code@gmail.com>
2019-10-29 21:39:49 +0700

    Tutorial DOM JavaScript - 06 DOM Selector untuk Memilih Banyak Element Bag 2

--- app.js
+++ app.js
@@ -11,8 +11,40 @@
 
 // document.getElementsByTagName
 
-const lis = document.getElementsByTagName('li');
-console.log(lis);
-console.log(lis[0]);
-lis[0].style.color = 'red';
-lis[2].textContent = 'Hello';
+// const lis = document.getElementsByTagName('li');
+// let lis = document.getElementsByTagName('li');
+
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
+// lis[2].textContent = 'Hello';
+
+// // Convert HTML Collection to Array
+// lis = Array.from(lis);
+
+// // lis.reverse();
+
+// lis.forEach(function(li, index){
+//     li.textContent = `${index}: Hello`;
+// });
+
+// console.log(lis);
+
+const items = document.querySelectorAll('ul.list-group li.list-group-item');
+
+items.forEach(function(item,index){
+     item.textContent = `${index} : Hello`;
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+  
+liOdd.forEach(function(li,index){
+    li.style.background = '#ccc';
+});
+
+for(let i = 0; i < liEven.length; i++){
+    liEven[i].style.background = '#f4f4f4';
+}
+
+console.log(items);
